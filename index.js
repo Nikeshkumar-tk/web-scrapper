e09a35fac02f818cc1aa916d349440952e325577
Nikeshkumar-tk <104181447+Nikeshkumar-tk@users.noreply.github.com>
2023-05-01 22:44:05 +0530

    Code cleaned

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-// import puppeteer from "puppeteer";
-// import MediaUtilities from "./lib/utils/media.js";
+//Workspace
 
 import PupputeerCore from "./lib/core/index.js"
 
 (async() => {
 
-
-// const browser = await puppeteer.launch()
-// const page = await browser.newPage()
-
-// await page.goto("https://learn-in-public.vercel.app/")
-
-// await page.screenshot({path:"screenshot.png"})
-
-// await browser.close()
 
 const scrapper = new PupputeerCore({pageUrl:"https://www.npmjs.com/package/puppeteer"})
 
